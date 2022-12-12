44cbe16b0372878fcfa0c2d628259836ab8311e5
Maulana Rheza <103730296+MaulanaRhezaArdiansyah@users.noreply.github.com>
2022-12-12 10:30:20 +0800

    second commit

--- task5.js
+++ task5.js
@@ -1,4 +1,12 @@
-const teks = "Denpasar";
-const teksBaru = teks.replace(/a/gi, "i");
+// CARA 1
+// const teks = "Denpasar";
+// const teksBaru = teks.replace(/a/gi, "i");
 
-console.log(teksBaru);
+// console.log(teksBaru);
+
+// CARA 2
+function ubahHurufAJadiI(teks) {
+  return teks.replace(/a/gi, "i");
+}
+
+console.log(ubahHurufAJadiI("Denpasar"));
